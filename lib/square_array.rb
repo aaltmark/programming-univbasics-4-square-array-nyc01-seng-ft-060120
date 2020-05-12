39f59bc76900a0adc38493c95d7648e4a0c8bdff
Adam Altmark <adamaltmark@gmail.com>
2020-05-12 14:24:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   counter = 0
   while counter < array.length do
-    puts Math.sqrt(array[counter])
+    new_array.push(Math.sqrt(array[counter]))
     counter += 1
   end
 end
